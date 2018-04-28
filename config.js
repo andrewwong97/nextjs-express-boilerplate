ca9a89bd56fb89ab004c4816fb357e21d994c0c4
Andrew Wong <wongandrew97@gmail.com>
2018-04-28 19:46:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.env = process.env.NODE_ENV || 'dev';
-config.port = process.env.PORT || '1337';
+config.port = process.env.PORT || '5000';
 config.host = process.env.HOST || '0.0.0.0';
 config.base = 'http://' + config.host + ':' + config.port ;
 
